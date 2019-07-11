4f09da0ab88e6a51c0cf2a0fd7591a683396c4dd
Chris Williams <cwill833@gmail.com>
2019-07-11 13:00:03 -0500

    routes back to homepage after signup

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -39,11 +39,13 @@ class App extends Component{
           }/>
           <Route exact path='/signup' render={({ history }) => 
             <SignupPage
+              history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
           <Route exact path='/login' render={({ history }) => 
             <LoginPage
+              history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
